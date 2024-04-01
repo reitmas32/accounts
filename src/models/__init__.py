7d4d2a29b64cf9b53344bab831213b3af1bcbf84
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-01 00:25:57 -0600

    feta: fix error of ruff

--- src/models/__init__.py
+++ src/models/__init__.py
@@ -1,5 +1,5 @@
-from .auth_email import AuthEmailModel
-from .auth_general_platform import AuthGeneralPlatformModel
-from .code import CodeModel
-from .user import UserModel
-from .user_login_methods import UserLoginMethodModel
+from .auth_email import AuthEmailModel  # noqa: F401
+from .auth_general_platform import AuthGeneralPlatformModel  # noqa: F401
+from .code import CodeModel  # noqa: F401
+from .user import UserModel  # noqa: F401
+from .user_login_methods import UserLoginMethodModel  # noqa: F401
