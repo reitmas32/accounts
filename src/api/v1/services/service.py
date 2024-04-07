779c7dc6bbf8fae96cefc1e3dd6eea0d71e99df1
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-07 02:18:41 -0600

    feat: add logic by dont repeat service_name

--- src/api/v1/services/service.py
+++ src/api/v1/services/service.py
@@ -1,28 +1,45 @@
 from uuid import UUID, uuid4
 
 from fastapi import status
+from sqlalchemy import and_, select
 
 from api.v1.services.proxies import RetrieveServiceSchema, ServiceProxy
 from api.v1.services.schemas import CreateServiceSchema, ListServiceSchema
 from core.settings import settings
 from core.utils.encryted import EncryptedController
-from core.utils.exceptions import FormException
+from core.utils.exceptions import FormException, ServiceNameException
 from core.utils.generic_views import (
     BaseService,
     ListBaseService,
     ObjectBaseService,
 )
 from core.utils.responses import create_simple_envelope_response
+from models.service import ServiceModel
 
 
 class CreateServicesService(BaseService):
     model = ServiceProxy
     schema = RetrieveServiceSchema
 
-    def _validate_request(self, payload: CreateServiceSchema):  # noqa: ARG002
+    def _validate_request(self, payload: CreateServiceSchema):
         success_request = True
 
-        # TODO make validations  # noqa: TD004
+        query = (
+            select(ServiceModel)
+            .where(
+                and_(
+                    ServiceModel.service_name == payload.service_name,
+                    ServiceModel.is_removed is False,
+                )
+            )
+            .limit(1)
+        )
+        service: ServiceModel = self.session.execute(query).scalars().first()
+
+        if service is not None:
+            raise ServiceNameException(
+                message=f"The service with name {service.service_name} is already in use"
+            )
 
         if self.request_errors["validations_success"] is False:
             raise FormException(field_errors=self.request_errors["validations_errors"])
