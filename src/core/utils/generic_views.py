5d3b153b794b0d06bdfa0fd8760f50deef701535
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-07 02:25:29 -0600

    fix: add params to response

--- src/core/utils/generic_views.py
+++ src/core/utils/generic_views.py
@@ -4,7 +4,7 @@ import logging
 from urllib.parse import parse_qs, urlencode, urlparse
 from uuid import UUID
 
-from fastapi import Request
+from fastapi import Request, status
 from fastapi_pagination.ext.sqlalchemy import paginate
 from sqlalchemy import JSON, DateTime, Select, select, types
 from sqlalchemy.dialects.postgresql import JSON as PJSON
@@ -34,6 +34,7 @@ class Base(DeclarativeBase, QueryModel):
     def __repr__(self) -> str:
         return self.__str__()
 
+
 class BaseService:
     model: Base
     schema: BaseSchema = BaseSchema
@@ -43,7 +44,10 @@ class BaseService:
         self.session = session
 
     def get_queryset(
-        self, unary_expressions: list | None = None, filters: dict | None = None, order_by: list | None = None
+        self,
+        unary_expressions: list | None = None,
+        filters: dict | None = None,
+        order_by: list | None = None,
     ):
         if order_by is None:
             order_by = []
@@ -52,11 +56,20 @@ class BaseService:
         if unary_expressions is None:
             unary_expressions = []
 
-        return select(self.model).where(*unary_expressions).filter_by(**filters).order_by(*order_by)
+        return (
+            select(self.model)
+            .where(*unary_expressions)
+            .filter_by(**filters)
+            .order_by(*order_by)
+        )
 
     def get_ordered_queryset(self, filters: dict):
-        unary_expressions, filters, order_by = ManagerFilter(model=self.model, filters=filters).manage_filters()
-        return self.get_queryset(unary_expressions=unary_expressions, filters=filters, order_by=order_by)
+        unary_expressions, filters, order_by = ManagerFilter(
+            model=self.model, filters=filters
+        ).manage_filters()
+        return self.get_queryset(
+            unary_expressions=unary_expressions, filters=filters, order_by=order_by
+        )
 
     def get_query(self, filters: dict):
         return self.get_ordered_queryset(filters)
@@ -83,7 +96,9 @@ class ObjectBaseService(BaseService):
 
 
 class ListBaseService(BaseService):
-    def _generate_pagination_links(self, current_page: int, page_size: int, total: int, request: Request):
+    def _generate_pagination_links(
+        self, current_page: int, page_size: int, total: int, request: Request
+    ):
         parsed_url = urlparse(str(request.url))
         params = parse_qs(parsed_url.query)
         total_pages = -(-total // page_size)
@@ -92,17 +107,23 @@ class ListBaseService(BaseService):
         next_page = None
         if current_page < total_pages:
             params["page"] = [current_page + 1]
-            next_page = parsed_url._replace(query=urlencode(params, doseq=True)).geturl()
+            next_page = parsed_url._replace(
+                query=urlencode(params, doseq=True)
+            ).geturl()
 
         # Calculate previous page link
         previous_page = None
         if current_page > 1:
             params["page"] = [current_page - 1]
-            previous_page = parsed_url._replace(query=urlencode(params, doseq=True)).geturl()
+            previous_page = parsed_url._replace(
+                query=urlencode(params, doseq=True)
+            ).geturl()
 
         return next_page, previous_page
 
-    def _apply_pagination(self, query: Select, pagination_params: PaginationParams) -> dict:
+    def _apply_pagination(
+        self, query: Select, pagination_params: PaginationParams
+    ) -> dict:
         page = paginate(self.session, query, pagination_params)
         return {"items": page.items, "total": page.total}
 
@@ -113,7 +134,9 @@ class ListBaseService(BaseService):
             "data": data,
         }
 
-    def _process_list(self, filters: dict, pagination_params: PaginationParams, request: Request) -> dict:
+    def _process_list(
+        self, filters: dict, pagination_params: PaginationParams, request: Request
+    ) -> dict:
         query = self.get_query(filters)
         page_info = self._apply_pagination(query, pagination_params)
 
@@ -135,19 +158,28 @@ class ListBaseService(BaseService):
         data = [self.transform_to_schema(item) for item in results]
         return self._build_response(count=len(data), data=data)
 
-    def list(self, filters: dict, pagination_params: PaginationParams, request: Request):
+    def list(
+        self, filters: dict, pagination_params: PaginationParams, request: Request
+    ):
         if pagination_params.size <= 0:
             data = self._process_all(filters)
         else:
             data = self._process_list(filters, pagination_params, request)
-        return self.create_response(**data)
+        return create_envelope_response(
+            **data,
+            message="Retrive service",
+            status_code=status.HTTP_200_OK,
+            successful=True,
+        )
 
 
 def validation_group(validation_function):
     def wrapper(self, *args, **kwargs):
         result_validation_function = validation_function(self, *args, **kwargs)
         returns_validation_function_number = (
-            len(result_validation_function) if type(result_validation_function) == tuple else 1
+            len(result_validation_function)
+            if type(result_validation_function) == tuple
+            else 1
         )
         if returns_validation_function_number <= 1:
             errors = result_validation_function
