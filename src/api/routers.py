d4f7679053f7250ae0afaf999f33087788d9e4f0
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-07 01:35:09 -0600

    feat: add services_endpoints to roots

--- src/api/routers.py
+++ src/api/routers.py
@@ -1,11 +1,13 @@
 from fastapi import APIRouter
 
 from api.health.endpoints import router as healthcheck_endpoints
-from api.v1.users.endpoints import router as user_endpoints
+from api.v1.services.endpoints import router as services_endpoints
+from api.v1.users.endpoints import router as users_endpoints
 from core.settings import settings
 
 api_healthcheck_router = APIRouter()
 api_healthcheck_router.include_router(healthcheck_endpoints)
 
-api_v1_router = APIRouter(prefix=f"/{settings.API_V1}")
-api_v1_router.include_router(user_endpoints)
+api_v1_router = APIRouter(prefix=f"/api/{settings.API_V1}")
+api_v1_router.include_router(users_endpoints)
+api_v1_router.include_router(services_endpoints)
