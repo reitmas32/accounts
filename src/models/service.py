ff73d764574ab594bd730e631b042f06896fe3d1
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-07 01:49:15 -0600

    feat: averride func as_dict

--- src/models/service.py
+++ src/models/service.py
@@ -7,3 +7,6 @@ class ServiceModel(BaseModelClass):
     __tablename__ = "services"
     api_key = Column(String, nullable=False)
     service_name = Column(String, nullable=False)
+
+    def as_dict(self):
+        return {"service_name": self.service_name.__str__(), "api_key": self.api_key}
