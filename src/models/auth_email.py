424275b9deb739180c4b7062472bcafcb071bb7b
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-07 01:49:24 -0600

    fix: format files

--- src/models/auth_email.py
+++ src/models/auth_email.py
@@ -8,9 +8,9 @@ from .user import UserModel
 
 class AuthEmailModel(BaseModelClass):
     __tablename__ = "auth_email"
-    user_id = Column(ForeignKey(UserModel.id,deferrable=True, initially="DEFERRED"),nullable=False,index=True)
-    email = Column(String,nullable=False)
-    password = Column(String,nullable=False)
-    active = Column(Boolean,nullable=False)
+    user_id = Column(ForeignKey(UserModel.id, deferrable=True, initially="DEFERRED"), nullable=False, index=True)
+    email = Column(String, nullable=False)
+    password = Column(String, nullable=False)
+    active = Column(Boolean, nullable=False)
 
     user = relationship(UserModel, primaryjoin="AuthEmailModel.user_id == UserModel.id")
