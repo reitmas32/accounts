381c735a159eeab7cee14791a0a5d2f022a71179
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-16 02:16:02 -0600

    feat: create funcs by authorization

--- src/core/utils/autorization.py
+++ src/core/utils/autorization.py
@@ -9,27 +9,56 @@ from core.utils.exceptions import NotAuthorizationException
 from models.service import ServiceModel
 
 
-def check_autorization(
+def check_authorization(
     request: Request,
-    X_API_Key: str = Header(...),  # noqa: N803
     X_Service_Name: str = Header(...),  # noqa: N803
+    X_API_Key: str = Header(...),  # noqa: N803
 ):
+    """
+    Check authorization headers for API requests.
+
+    This function checks the authorization headers provided in the request for API authentication.
+
+    Args:
+        request (Request): FastAPI request object.
+        X_Service_Name (str): Service name header.
+        X_API_Key (str): API key header.
+
+    Returns:
+        bool: True if authorization is successful, False otherwise.
+    """
     if request.url.path.startswith("/api/v1/services"):
-        return _check_root_autorization(
+        return _check_root_authorization(
             X_API_Key=X_API_Key,
             X_Service_Name=X_Service_Name,
         )
-    return _check_general_autorization(
+    return _check_general_authorization(
         request=request,
         X_API_Key=X_API_Key,
         X_Service_Name=X_Service_Name,
     )
 
 
-def _check_root_autorization(
+
+def _check_root_authorization(
     X_API_Key: str = "",  # noqa: N803
     X_Service_Name: str = "",  # noqa: N803,
 ):
+    """
+    Check root authorization for API requests.
+
+    This function checks the root authorization headers provided in the request for API authentication.
+
+    Args:
+        X_API_Key (str): API key header.
+        X_Service_Name (str): Service name header.
+
+    Returns:
+        bool: True if root authorization is successful, False otherwise.
+
+    Raises:
+        NotAuthorizationException: If root authorization fails.
+    """
     if (
         X_API_Key != settings.ROOT_API_KEY
         or X_Service_Name != settings.ROOT_SERVICE_NAME
@@ -38,11 +67,28 @@ def _check_root_autorization(
     return True
 
 
-def _check_general_autorization(
+
+def _check_general_authorization(
     request: Request,
     X_API_Key: str = "",  # noqa: N803
     X_Service_Name: str = "",  # noqa: N803,
 ):
+    """
+    Check general authorization for API requests.
+
+    This function checks the general authorization headers provided in the request for API authentication.
+
+    Args:
+        request (Request): FastAPI request object.
+        X_API_Key (str): API key header.
+        X_Service_Name (str): Service name header.
+
+    Returns:
+        bool: True if general authorization is successful, False otherwise.
+
+    Raises:
+        NotAuthorizationException: If general authorization fails.
+    """
     with use_database_session() as session:
         query = (
             select(ServiceModel)
