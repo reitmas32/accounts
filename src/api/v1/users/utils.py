803c15c83689643f17bd2af56e2836e30c3da72f
rafazr-jalo <rafazr.jalo@gmail.com>
2024-04-16 02:23:58 -0600

    feat: add docs to class

--- src/api/v1/users/utils.py
+++ src/api/v1/users/utils.py
@@ -19,18 +19,54 @@ from models.code import CodeTypeEnum
 
 
 class CodeManager:
+    """
+    Manager class for handling code-related operations.
+
+    This manager is responsible for generating, creating, sending, and validating codes used in the authentication process.
+
+    Args:
+        session: Database session for interacting with the data.
+
+    Attributes:
+        repository_code: Repository for code data operations.
+        manager_email: Email manager for sending emails.
+    """
+
     def __init__(self, session: Session):
+        """
+        Initialize the CodeManager.
+
+        Args:
+            session: Database session for interacting with the data.
+        """
         self.repository_code = RepositoryCode(session=session)
         self.manager_email: SendEmailAbstract = get_current_manager_email_to_app_standard()
 
     def generate_code(self, length):
+        """
+        Generate a random code.
+
+        Args:
+            length (int): Length of the generated code.
+
+        Returns:
+            str: Generated code.
+        """
         return "".join(random.choices(string.digits, k=length))  # noqa: S311
 
     def create_and_send_code(self, email, user_name, code_type: CodeTypeEnum):
-        code_lenght = settings.LENGHT_CODE_VALIDATE_EMAIL
+        """
+        Generate, create, and send a code via email.
+
+        Args:
+            email (str): Email address to send the code to.
+            user_name (str): Username associated with the email address.
+            code_type (CodeTypeEnum): Type of code to generate and send.
+        """
+        code_length = settings.LENGHT_CODE_VALIDATE_EMAIL
         if code_type.value == CodeTypeEnum.TWO_FACTOR.value:
-            code_lenght = settings.LENGHT_CODE_2FA
-        new_code = self.generate_code(length=code_lenght)
+            code_length = settings.LENGHT_CODE_2FA
+        new_code = self.generate_code(length=code_length)
         if code_type.value == CodeTypeEnum.TWO_FACTOR.value:
             subject_text, message_text = get_data_for_email_two_factor(user_name=user_name, two_factor_code=new_code)
         else:
@@ -45,6 +81,17 @@ class CodeManager:
         )
 
     def validate_code(self, email, code, code_type: CodeTypeEnum):
+        """
+        Validate a code.
+
+        Args:
+            email (str): Email address associated with the code.
+            code (str): Code to validate.
+            code_type (CodeTypeEnum): Type of code to validate.
+
+        Returns:
+            bool: True if the code is valid, False otherwise.
+        """
         time_seconds_expire = settings.TIME_SECONDS_EXPIRE_CODE_2FA
         if code_type.value == CodeTypeEnum.ACCOUNT_ACTIVATION.value:
             time_seconds_expire = settings.TIME_SECONDS_EXPIRE_CODE_VALIDATE_EMAIL
@@ -57,3 +104,4 @@ class CodeManager:
             self.repository_code.update_field_by_id(id=code_entry.id, field_name="used_at", new_value=current_time)
             return True
         return False
+
