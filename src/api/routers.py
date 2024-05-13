cbc1a58c4b20b8b2cef586d36d0dffe61710f4f9
rafazr-jalo <rafazr.jalo@gmail.com>
2024-05-12 20:20:49 -0600

    feat: extract logic of authorizatuion to microservices

--- src/api/routers.py
+++ src/api/routers.py
@@ -5,7 +5,6 @@ from api.v1.codes.crud.endpoints import router as codes_crud_endpoints
 from api.v1.emails.crud.endpoints import router as emails_crud_endpoints
 from api.v1.emails.logic.endpoints import router as emails_logic_endpoints
 from api.v1.login_methods.endpoints import router as login_methods_crud_endpoints
-from api.v1.services.endpoints import router as services_crud_endpoints
 from api.v1.users.crud.endpoints import router as users_crud_endpoints
 from core.settings import settings
 
@@ -14,7 +13,6 @@ api_healthcheck_router.include_router(healthcheck_endpoints)
 
 api_v1_router = APIRouter(prefix=f"/api/{settings.API_V1}")
 api_v1_router.include_router(users_crud_endpoints)
-api_v1_router.include_router(services_crud_endpoints)
 api_v1_router.include_router(codes_crud_endpoints)
 api_v1_router.include_router(emails_crud_endpoints)
 api_v1_router.include_router(login_methods_crud_endpoints)
--- src/api/v1/services/filters.py
+++
@@ -1,5 +0,0 @@
-from core.utils.responses import FilterBaseSchema
-
-
-class FilterServicesSchema(FilterBaseSchema):
-    service_name: str | None = None
