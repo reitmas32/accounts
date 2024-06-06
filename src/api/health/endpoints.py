d77a64bec7ff6fac45f59ca7a2d93b98d849b1b3
rafazr-jalo <rafazr.jalo@gmail.com>
2024-06-05 19:53:32 -0600

    feat: change import of logger

--- src/api/health/endpoints.py
+++ src/api/health/endpoints.py
@@ -1,7 +1,7 @@
 from fastapi import APIRouter, status
-from loguru import logger
 
 from core.settings import settings
+from core.utils.logger import logger
 from core.utils.responses import EnvelopeResponse
 
 router = APIRouter(tags=["Health Check"])
