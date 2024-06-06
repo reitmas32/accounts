42b45d4e7a97aa293ebe0e61a85062ede13e2eb9
rafazr-jalo <rafazr.jalo@gmail.com>
2024-06-05 19:56:46 -0600

    feat: create AuthServiceClient

---
+++ src/core/clients/auth/client.py
@@ -0,0 +1,46 @@
+# myapp/middlewares.py
+
+
+from http import HTTPStatus
+from typing import TYPE_CHECKING
+
+from core.clients.auth.schemas import AuthSchema
+from core.settings import settings
+from core.utils.logger import logger
+from shared.client import APIRestClient
+from shared.constants import MethodType, ReturnType
+from shared.exceptions import APIExceptionError, NotAuthorizedError
+
+if TYPE_CHECKING:
+    import requests
+
+
+class AuthServiceClient(APIRestClient):
+    BASE_URL: str = (
+        settings.AUTH_SERVICE_API_HOST
+        + "/api"
+        + f"/{settings.AUTH_SERVICE_API_VERSION}"
+        + f"/{settings.AUTH_SERVICE_API_PREFIX}"
+    )
+
+    def validate(self, resource: str, payload: AuthSchema):
+        url = f"{self.BASE_URL}/validate"
+        try:
+            logger.info(f"Request to URL: {url}")
+            response: requests.Response = self._request(
+                url,
+                method=MethodType.POST,
+                data=payload.model_dump_json(exclude_none=True),
+                return_type=ReturnType.FULL,
+            )
+            logger.info(f"Response: {response}")
+            if response is None:
+                logger.error(f"Error to Auth service in {url}")
+            if response.status_code != HTTPStatus.CREATED:
+                logger.error("Dont Autorization Service to resource")
+                raise NotAuthorizedError(resource)
+
+        except APIExceptionError as e:
+            logger.info(f"Response: {e.response}")
+            response = None
+        return response
