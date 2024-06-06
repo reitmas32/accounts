5a4e6dad6b918e88935dbb8855f3952f2a0dd6a3
rafazr-jalo <rafazr.jalo@gmail.com>
2024-06-05 19:56:05 -0600

    feat: remove logic of check_authorization

--- src/core/utils/autorization.py
+++ src/core/utils/autorization.py
@@ -11,27 +11,4 @@ def check_authorization(
     X_Service_Name: str = Header(...),  # noqa: N803
     X_API_Key: str = Header(...),  # noqa: N803
 ):
-    """
-    Check authorization headers for API requests.
-
-    This function checks the authorization headers provided in the request for API authentication.
-
-    Args:
-        request (Request): FastAPI request object.
-        X_Service_Name (str): Service name header.
-        X_API_Key (str): API key header.
-
-    Returns:
-        bool: True if authorization is successful, False otherwise.
-    """
-    url = settings.AUTH_MANAGER_URL
-    headers = {"accept": "application/json", "Content-Type": "application/json"}
-    data = {
-        "service_name": X_Service_Name,
-        "api_key": X_API_Key,
-    }
-
-    response = requests.post(url, headers=headers, json=data, timeout=3000)
-    if response.status_code != status.HTTP_201_CREATED:
-        raise NotAuthorizationException(resource=request.url.path)
-    return True
+    pass
