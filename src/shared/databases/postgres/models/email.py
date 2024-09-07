e051b77c1ec5ab93c63b549a9d988fe366d598da
rafazr-jalo <rafazr.jalo@gmail.com>
2024-09-07 11:12:54 -0600

    refactor: change dir of models

---
+++ src/shared/databases/postgres/models/email.py
@@ -0,0 +1,44 @@
+from sqlalchemy import Column, ForeignKey, String
+from sqlalchemy.orm import relationship
+
+from shared.databases.postgres.models.base_model import BaseModelClass
+
+from .user import UserModel
+
+
+class EmailModel(BaseModelClass):
+    """
+    Model for storing email information linked to a user. This class stores a user's email
+    and its associated password, allowing the system to track email accounts and their
+    relationship to users.
+
+    Inherits common fields and behavior from BaseModelClass.
+
+    Attributes:
+    -----------
+    - `user_id`: Foreign key linking the email record to the corresponding user in the `UserModel`.
+    - `email`: The email address (string) associated with the user.
+    - `password`: The password linked to the email account (string).
+
+    Relationships:
+    --------------
+    - `user`: Establishes a relationship with the `UserModel`, linking the email to the user.
+    """
+
+    __tablename__ = "emails"  # Specifies the table name in the database
+
+    user_id = Column(
+        ForeignKey(UserModel.id, deferrable=True, initially="DEFERRED"),
+        nullable=False,
+        index=True,
+    )
+    """Foreign key to the `UserModel`, indicating which user this email belongs to. Indexed for faster lookup."""
+
+    email = Column(String, nullable=False)
+    """String representing the email address associated with the user. This field cannot be null."""
+
+    password = Column(String, nullable=False)
+    """String representing the password linked to the email account. This field cannot be null."""
+
+    user = relationship(UserModel, primaryjoin="EmailModel.user_id == UserModel.id")
+    """SQLAlchemy relationship to the `UserModel`, allowing access to user details related to this email."""
