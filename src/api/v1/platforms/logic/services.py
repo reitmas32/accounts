f32d747452f1e88378c098e5ca81021619ec1c3b
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-20 00:40:51 -0600

    refactor: mode SAGAContrroller to shared

--- src/api/v1/platforms/logic/services.py
+++ src/api/v1/platforms/logic/services.py
@@ -6,9 +6,9 @@ from api.v1.platforms.logic.steps import (
     CreateUserStep,
     FindPlatformUserStep,
 )
-from core.controllers.saga.controller import SagaController
 from core.utils.generic_views import BaseService
 from core.utils.responses import create_simple_envelope_response
+from shared.app.controllers import SagaController
 from shared.app.handlers.jwt import JWTHandler
 from shared.databases.postgres.models.user import UserModel
 
