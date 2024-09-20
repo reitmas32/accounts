ba292e3436790626997eaa1088634710bf07b520
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-19 23:21:33 -0600

    fix: solve error in decode data of response

--- src/core/middlewares/webhook.py
+++ src/core/middlewares/webhook.py
@@ -1,7 +1,10 @@
+import asyncio
 import json
+from collections.abc import Awaitable, Callable
 
 import requests
 from fastapi import Request, Response
+from fastapi.concurrency import iterate_in_threadpool
 from fastapi.responses import StreamingResponse
 from pydantic import BaseModel
 from starlette.middleware.base import BaseHTTPMiddleware
@@ -9,6 +12,7 @@ from starlette.middleware.base import BaseHTTPMiddleware
 from core.settings import settings
 from core.utils.logger import logger
 
+background_tasks = set()
 
 class WebHookDTO(BaseModel):
     resource: str
@@ -16,42 +20,46 @@ class WebHookDTO(BaseModel):
 
 
 class CallWebHookMiddleware(BaseHTTPMiddleware):
-    async def dispatch(self, request: Request, call_next):
+    async def dispatch(
+        self,
+        request: Request,
+        call_next: Callable[[Request], Awaitable[StreamingResponse]],
+    ) -> Response:
         response = await call_next(request)
-
         path = request.url.path
-        if path in settings.WEBHOOK_SIGNALS and isinstance(response, StreamingResponse):
-            # Consumir el contenido de la respuesta
-            response_body = b""
-            async for chunk in response.body_iterator:
-                response_body += chunk
-
-            # Intentar decodificar el cuerpo de la respuesta como JSON
-            try:
-                json_response = json.loads(response_body.decode())
-                data_webhook = WebHookDTO(resource=path, response=json_response)
-                headers = {
-                    "accept": "application/json",
-                    "Content-Type": "application/json",
-                }
-
-                requests.post(  # noqa: ASYNC210
-                    settings.WEBHOOK,
-                    headers=headers,
-                    json=data_webhook.model_dump(),
-                    timeout=0.001,
-                )
-
-            except json.JSONDecodeError:
-                logger.error("Error call the webhook")
-            except requests.Timeout:
-                pass
-
-            return Response(
-                content=response_body,
-                status_code=response.status_code,
-                headers=dict(response.headers),
-                media_type=response.media_type,
+        if path in settings.WEBHOOK_SIGNALS:
+            response_body = [section async for section in response.body_iterator]
+            response.body_iterator = iterate_in_threadpool(iter(response_body))
+            task = asyncio.create_task(
+                CallWebHookMiddleware.call_webhook(
+                    resource=request.base_url.path,
+                    response=response_body[0].decode(),
+                ),
             )
 
+            background_tasks.add(task)
+
+            task.add_done_callback(background_tasks.discard)
         return response
+
+    @staticmethod
+    async def call_webhook(resource: str, response: dict | None = None):
+        # Simula una tarea que toma tiempo
+
+        data_webhook = WebHookDTO(resource=resource, response=json.loads(response))
+        headers = {
+            "accept": "application/json",
+            "Content-Type": "application/json",
+        }
+        try:
+            requests.post(  # noqa: ASYNC210
+                settings.WEBHOOK,
+                headers=headers,
+                json=data_webhook.model_dump(),
+                timeout=0.001,
+            )
+        except json.JSONDecodeError:
+            logger.error("Error call the webhook")
+        except requests.Timeout:
+            pass
+
