8583d9ef0edfcf000129fd49bc0f75ad60c44b5b
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-20 12:10:59 -0600

    feat: implemnet the use_case

--- src/api/v1/users/presentation/endpoints/create.py
+++ src/api/v1/users/presentation/endpoints/create.py
@@ -1,5 +1,9 @@
+
 from fastapi import Request, status
 
+from api.v1.users.domain.entities.user import UserEntity
+from api.v1.users.domain.usecase.create import CreateUserUseCase
+from api.v1.users.infrastructure.repositories.postgres.user import UserRepository
 from api.v1.users.presentation.dtos import CreateUserDto
 from core.utils.logger import logger
 from core.utils.responses import (
@@ -20,8 +24,15 @@ async def create(
     payload: CreateUserDto,
 ):
     logger.info("Create User")
+
+    entity: UserEntity = UserEntity(**payload.model_dump())
+
+    use_case = CreateUserUseCase(repository=UserRepository())
+
+    new_entity: UserEntity = use_case.execute(payload=entity) # el caso de uso debe genera una Response intermedia o porlomenos retornar el stataus code
+
     return EnvelopeResponse(
-        data=payload.model_dump(),
+        data=new_entity.model_dump(),
         success=True,
         response_code=status.HTTP_201_CREATED,
     )
