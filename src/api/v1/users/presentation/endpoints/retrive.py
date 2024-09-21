4d177d51020efb4b3b9afdc7d4ee94b200d4b59f
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-20 19:21:14 -0600

    feat: add RetriveUserUseCase to endpoint

--- src/api/v1/users/presentation/endpoints/retrive.py
+++ src/api/v1/users/presentation/endpoints/retrive.py
@@ -1,8 +1,11 @@
+from typing import TYPE_CHECKING
 from uuid import UUID
 
 from fastapi import Depends, Request, status
 from sqlalchemy.orm import Session
 
+from api.v1.users.domain.usecase.retrive import RetriveUserUseCase
+from api.v1.users.infrastructure.repositories.postgres.user import UserRepository
 from core.settings import log
 from core.settings.database import get_session
 from core.utils.responses import (
@@ -11,21 +14,34 @@ from core.utils.responses import (
 
 from .routers import router
 
+if TYPE_CHECKING:
+    from api.v1.users.domain.entities.user import UserEntity
+
 
 @router.get(
-    "/{user_id}",
+    "/{id}",
     summary="Returns the data of the authenticated user",
     status_code=status.HTTP_200_OK,
     response_model=EnvelopeResponse,
 )
 async def retrieve_one(
     request: Request,
-    user_id: UUID,
+    id: UUID,
     session: Session = Depends(get_session),
 ):
     log.info("Get User")
+
+    use_case = RetriveUserUseCase(repository=UserRepository())
+
+    entity: UserEntity | None = use_case.execute(id=id)
+
+    data = None
+
+    if entity is not None:
+        data = entity.model_dump()
+
     return EnvelopeResponse(
-        data={"user_id": user_id},
+        data=data,
         response_code=status.HTTP_200_OK,
         success=True,
     )
