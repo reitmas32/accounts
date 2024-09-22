73aa5414b03c517e7863ea5574aa8a7f6eaaa88e
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-22 15:49:17 -0600

    feat: add use of especific errors in JWTHandler

--- src/shared/app/handlers/jwt.py
+++ src/shared/app/handlers/jwt.py
@@ -6,6 +6,8 @@ from pydantic import ValidationError
 
 from core.settings import settings
 from core.utils.responses import get_current_date_time_to_app_standard
+from shared.app.errors.invalid.jwt_expider import JWTExpiredError
+from shared.app.errors.invalid.jwt_invalid import JWTInvalidError
 from shared.presentation.schemas.jwt import JWTSchema
 
 
@@ -23,8 +25,8 @@ class JWTHandler:
             payload = decode(token, settings.PUBLIC_KEY_JWT, algorithms=[settings.ALGORITHM_JWT.value])
             return JWTSchema(**payload)
         except ExpiredSignatureError:
-            raise ValueError("Token has expired")
+            raise JWTExpiredError("Token has expired")
         except InvalidTokenError:
-            raise ValueError("Invalid token")
+            raise JWTInvalidError("Invalid token")
         except ValidationError as e:
-            raise ValueError(f"Invalid token data: {e}")
+            raise JWTInvalidError(f"Invalid token data: {e}")
