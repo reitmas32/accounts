be4f1bb38d05f025a14201ddb204893db48b5e19
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-22 00:22:11 -0600

    feat: add fileds by JWTSchema

--- src/shared/presentation/schemas/jwt.py
+++ src/shared/presentation/schemas/jwt.py
@@ -1,5 +1,10 @@
 from pydantic import BaseModel
 
+from shared.app.enums.user_login_methods import UserLoginMethodsTypeEnum
+
 
 class JWTSchema(BaseModel):
     user_id: str
+    entity_id: str
+    entity_type: UserLoginMethodsTypeEnum
+
