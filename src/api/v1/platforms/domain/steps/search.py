684ea94c3dbe0df8956c3a0ebd1007d6778d4028
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-22 01:08:50 -0600

    feat: add platform by search

--- src/api/v1/platforms/domain/steps/search.py
+++ src/api/v1/platforms/domain/steps/search.py
@@ -1,21 +1,30 @@
-
-
 from shared.app.controllers.saga.controller import StepSAGA
+from shared.app.enums.platform_login import PlatformsLogin
 from shared.databases.infrastructure.repository import RepositoryInterface
 
 
-class SearchPlatformByExternalIdStep(StepSAGA):
-    def __init__(self, external_id: str, repository: RepositoryInterface):
-
+class SearchPlatformStep(StepSAGA):
+    def __init__(
+        self,
+        external_id: str,
+        platform: PlatformsLogin,
+        repository: RepositoryInterface,
+    ):
         self.external_id = external_id
         self.repository = repository
+        self.platform = platform
 
     def __call__(self, payload: None = None, all_payloads: dict | None = None):  # noqa: ARG002
-        platform = self.repository.get_by_attributes(filters={"external_id": self.external_id})
-        if platform is None:
-            return []
+        platform = self.repository.get_by_attributes(
+            filters={
+                "external_id": self.external_id,
+                "platform": self.platform,
+            }
+        )
+        if platform is None or len(platform) == 0:
+            return None
 
-        return platform
+        return platform[0]
 
     def rollback(self):
         """
