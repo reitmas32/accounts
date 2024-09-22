3eb0f13af1cdca65870911d8017f38d7c860ecca
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-21 20:58:36 -0600

    feat: update PasswordError params by message

--- src/shared/app/errors/invalid/password.py
+++ src/shared/app/errors/invalid/password.py
@@ -7,7 +7,7 @@ class PasswordError(BaseError):
     external_code = StatusCodes.HTTP_422_UNPROCESSABLE_ENTITY
     internal_code = StatusCodes.APP_INVALID_PASSWORD_FORMAT
 
-    def __init__(self) -> None:
-        self.message = "The password must have [A-Za-z0-9 &%$*?¿¡!]"
+    def __init__(self, message: str = "The password must have", value: str = "[A-Za-z0-9 &%$*?¿¡!]") -> None:
+        self.message = f"{message} {value}"
         super().__init__(self.message)
         logger.error(self.message)
