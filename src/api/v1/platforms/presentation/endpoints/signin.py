62b016c9d4c1e2968f38e844b4d9a405b7bf3ceb
reitmas32 <rafa.zamora.rals@gmail.com>
2024-09-22 01:00:16 -0600

    feat: create signin by platform

---
+++ src/api/v1/platforms/presentation/endpoints/signin.py
@@ -0,0 +1,45 @@
+from fastapi import status
+
+from api.v1.login_methods.infrastructure.repositories.postgres.login_method import (
+    LoginMethodRepository,
+)
+from api.v1.platforms.domain.entities.singup import SignupPlatformEntity
+from api.v1.platforms.domain.usecase.singin import SignInPlatformUseCase
+from api.v1.platforms.infrastructure.repositories.postgres.user import (
+    PlatformRepository,
+)
+from api.v1.platforms.presentation.dtos.signin import SigninPlatformDto
+from api.v1.platforms.presentation.endpoints.routers import router
+from core.utils.logger import logger
+from core.utils.responses import (
+    EnvelopeResponse,
+)
+
+
+@router.post(
+    "/signin",
+    summary="Signin By Platform",
+    status_code=status.HTTP_201_CREATED,
+    response_model=EnvelopeResponse,
+)
+async def signup(
+    payload: SigninPlatformDto,
+):
+    logger.info("Signin By Platform")
+
+    entity: SignupPlatformEntity = SignupPlatformEntity(**payload.model_dump())
+
+    use_case = SignInPlatformUseCase(
+        repository=PlatformRepository(),
+        login_method_repository=LoginMethodRepository(),
+    )
+
+    jwt = use_case.execute(
+        payload=entity
+    )  # el caso de uso debe genera una Response intermedia o porlomenos retornar el stataus code
+
+    return EnvelopeResponse(
+        data=jwt,
+        success=True,
+        response_code=status.HTTP_201_CREATED,
+    )
