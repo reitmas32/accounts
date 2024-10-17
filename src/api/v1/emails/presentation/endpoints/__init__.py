b49ade6cd1a4b6f9eddf6cdaa0986b0187036ac2
reitmas32 <rafa.zamora.rals@gmail.com>
2024-10-17 11:54:50 -0600

    feat: create endpoint signup with email

--- src/api/v1/emails/presentation/endpoints/__init__.py
+++ src/api/v1/emails/presentation/endpoints/__init__.py
@@ -3,4 +3,5 @@ from .delete import *  # noqa: F403
 from .list import *  # noqa: F403
 from .retrive import *  # noqa: F403
 from .routers import *  # noqa: F403
+from .signup import *  # noqa: F403
 from .update import *  # noqa: F403
---
+++ src/api/v1/emails/presentation/endpoints/signup.py
@@ -0,0 +1,57 @@
+from fastapi import Request, status
+
+from api.v1.codes.infrastructure.repositories.postgres.user import CodeRepository
+from api.v1.emails.domain.entities.email import EmailEntity
+from api.v1.emails.domain.usecase.create import SignUpWithEmailUseCase
+from api.v1.emails.infrastructure.repositories.postgres.email import EmailRepository
+from api.v1.emails.presentation.dtos import SignupEmailDto
+from api.v1.users.infrastructure.repositories.postgres.user import UserRepository
+from core.utils.logger import logger
+from core.utils.responses import (
+    EnvelopeResponse,
+)
+
+from .routers import router
+
+
+@router.post(
+    "/signup",
+    summary="SignUp user with email",
+    status_code=status.HTTP_201_CREATED,
+    response_model=EnvelopeResponse,
+)
+async def signup(
+    request: Request,
+    payload: SignupEmailDto,
+):
+    """
+    Create a user registration via email.
+
+    This endpoint allows the creation of a user account using email as the authentication method.
+
+    Args:
+        request (Request): FastAPI request object.
+        payload (SignupEmailSchema): Data payload containing email signup information.
+        _: Dependency to check authorization (ignored).
+
+    Returns:
+        dict: Envelope response containing user data, message, and status code.
+    """
+    logger.info("Create Email")
+
+    entity: EmailEntity = EmailEntity(**payload.model_dump())
+
+    use_case = SignUpWithEmailUseCase(
+        email_repository=EmailRepository(),
+        user_repository=UserRepository(),
+        code_repository=CodeRepository(),
+        user_name=payload.user_name,
+    )
+
+    new_entity: EmailEntity = use_case.execute(payload=entity)
+
+    return EnvelopeResponse(
+        data=new_entity.model_dump(),
+        success=True,
+        response_code=status.HTTP_201_CREATED,
+    )
