80f2009ecd87f69866cc71e43903cfe3cc691f00
reitmas32 <rafa.zamora.rals@gmail.com>
2024-12-02 10:33:18 -0600

    feat: add session: Depends(get_session) in users endpoints

--- src/api/v1/users/presentation/endpoints/create.py
+++ src/api/v1/users/presentation/endpoints/create.py
@@ -1,10 +1,13 @@
 
 from fastapi import status
+from fastapi.params import Depends
+from sqlalchemy.orm import Session
 
 from api.v1.users.presentation.dtos import CreateUserDto
 from context.v1.users.domain.entities.user import UserEntity
 from context.v1.users.domain.usecase.create import CreateUserUseCase
 from context.v1.users.infrastructure.repositories.postgres.user import UserRepository
+from core.settings.database import get_session
 from core.utils.logger import logger
 from shared.app.status_code import StatusCodes
 from shared.presentation.schemas.envelope_response import ResponseEntity
@@ -20,12 +23,14 @@ from .routers import router
 )
 async def create(
     payload: CreateUserDto,
+    session: Session = Depends(get_session)
+
 ):
     logger.info("Create User")
 
     entity: UserEntity = UserEntity(**payload.model_dump())
 
-    use_case = CreateUserUseCase(repository=UserRepository())
+    use_case = CreateUserUseCase(repository=UserRepository(session=session))
 
     new_entity: UserEntity = use_case.execute(payload=entity)
 
